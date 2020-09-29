aa7e8107f7ba31c9ba0298f3e9890b794d27a5a3
tdeso <48097413+tdeso@users.noreply.github.com>
2020-09-29 14:25:29 +0200

    Delete testnew.sh

--- testnew.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-read -p "Enter your VPS public IP: "  PUBLIC_IP 
-sudo bash -c 'cat <<EOF > /etc/.avaxnodeconf
-ARG1=--public-ip=$PUBLIC_IP
-ARG2=--snow-quorum-size=14
-ARG3=--snow-virtuous-commit-threshold=15
-EOF'
