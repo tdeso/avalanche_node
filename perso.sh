e5671baa165fa6fb250f2914725c93e24e20c0fd
tdeso <48097413+tdeso@users.noreply.github.com>
2020-09-29 14:17:51 +0200

    Update perso.sh

--- perso.sh
+++ perso.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-sudo bash -c 'cat <<EOF > $HOME/perso/testconf
+sudo read -p "Enter your VPS public IP: "  PUBLIC_IP 
+sudo bash -c 'cat <<EOF > /etc/.avaxnodeconf
 ARG1=--public-ip=$PUBLIC_IP
 ARG2=--snow-quorum-size=14
 ARG3=--snow-virtuous-commit-threshold=15
 EOF'
+
