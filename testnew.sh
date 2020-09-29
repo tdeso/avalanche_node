6f1774c83d96c0a94c2b46d81544450621ab2af1
tdeso <48097413+tdeso@users.noreply.github.com>
2020-09-29 14:23:40 +0200

    Update testnew.sh

--- testnew.sh
+++ testnew.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo read -p "Enter your VPS public IP: "  PUBLIC_IP 
+read -p "Enter your VPS public IP: "  PUBLIC_IP 
 sudo bash -c 'cat <<EOF > /etc/.avaxnodeconf
 ARG1=--public-ip=$PUBLIC_IP
 ARG2=--snow-quorum-size=14
