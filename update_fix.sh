759e3411e848f5c613c8c7ace4deba85c3157a33
tdeso <48097413+tdeso@users.noreply.github.com>
2020-09-29 14:58:39 +0200

    Update update_fix.sh

--- update_fix.sh
+++ update_fix.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-################################################
-# Simple Bash script to update an Avalanche Node
-# ##############################################
+#########################################
+# Bash script to update an Avalanche Node
+#########################################
 
 echo "export GOROOT=/usr/local/go" >> $HOME/.bash_profile
 echo "export GOPATH=$HOME/go" >> $HOME/.bash_profile
@@ -11,11 +11,21 @@ source $HOME/.bash_profile
 go env -w GOPATH=$HOME/go
 
 sudo systemctl stop avalanchenode
+sudo systemctl disable avalanche
+
 sudo systemctl stop avalanche
+sudo systemctl disable avalanche
+
 sudo systemctl stop avaxnode
+
+sudo sed -i -e 's/avalanchenode/avaxnode/g' $HOME/avalanche-discord.py
 
 echo '### Updating packages...'
 sudo apt-get -y update
+
+echo '### Fixing the databse...'
+cd ~/.avalanchego
+rm -rf db
 
 echo '### Updating the repository...'
 cd $GOPATH/src/github.com/ava-labs/avalanchego
@@ -23,8 +33,10 @@ git pull
 
 echo '### Updating Avalanche node service...'
 ./scripts/build.sh
-sudo systemctl restart avaxnode
+sudo systemctl start avaxnode
 
-echo 'Done !'
-echo 'Type the following command to monitor the AVA node service:'
+echo '!!!!!!!!!!'
+echo '!! DONE !!'
+echo '!!!!!!!!!!'
+echo 'Type the following command to monitor the Avalanche node service:'
 echo 'sudo systemctl status avaxnode'
