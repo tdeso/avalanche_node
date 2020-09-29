ea8bdab2369d90d85a79cbbb28ddc7643373ee41
tdeso <48097413+tdeso@users.noreply.github.com>
2020-09-29 14:20:33 +0200

    Create testnew.sh

---
+++ testnew.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo read -p "Enter your VPS public IP: "  PUBLIC_IP 
+sudo bash -c 'cat <<EOF > /etc/.avaxnodeconf
+ARG1=--public-ip=$PUBLIC_IP
+ARG2=--snow-quorum-size=14
+ARG3=--snow-virtuous-commit-threshold=15
+EOF'
