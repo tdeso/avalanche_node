89785cadc66f9cc3fb5d87f670d9c2f341153f41
tdeso <48097413+tdeso@users.noreply.github.com>
2020-10-01 18:05:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-#######################################
-# Bash script to install dependencies in Ubuntu 18.04.x LTS
-# for https://www.avalabs.org/ Nodes
-# ######################################
     
 echo '    /\ \    / /\   | |        /\   | \ | |/ ____| |  | |  ____|
 echo '   /  \ \  / /  \  | |       /  \  |  \| | |    | |__| | |__   
@@ -15,23 +10,20 @@ echo '/_/    \_\/_/    \_\______/_/    \_\_| \_|\_____|_|  |_|______|
 echo '### Updating packages...'
 sudo apt-get update -y
 
+echo '### Importing scripts...'
+cd $HOME && mkdir bin
+git clone https://github.com/tdeso/avalanche_node.git
+mv $HOME/avalanche_node/*.sh $HOME/bin/ && chmod +x $HOME/bin/*.sh && rm -rf avalanche_node
+
 echo '### Installing Go...'
 wget https://dl.google.com/go/go1.13.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.13.linux-amd64.tar.gz
 echo "export GOROOT=/usr/local/go" >> $HOME/.bash_profile
 echo "export GOPATH=$HOME/go" >> $HOME/.bash_profile
-echo "export PATH=$GOPATH/bin:$GOROOT/bin:$PATH" >> $HOME/.bash_profile
+echo "export PATH=$GOPATH/bin:$GOROOT/bin:$HOME/bin:$PATH" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 go env -w GOPATH=$HOME/go
 go version
-
-
-#echo '### Installing Nodejs...'
-#sudo apt-get update -y
-#curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-#sudo apt-get -y install nodejs
-#sudo apt-get -y install build-essential
-
 
 echo '### Cloning avalanchego directory...'
 cd $HOME
@@ -42,16 +34,14 @@ cd $GOPATH/src/github.com/ava-labs/avalanchego
 ./scripts/build.sh
 
 echo '### Creating Avalanche node service...'
-
-
 #sudo read -p "Enter your VPS public IP: "  PUBLIC_IP 
-sudo bash -c 'cat <<EOF > /etc/.avaxnodeconf
+sudo bash -c 'cat <<EOF > /etc/.avalanche.conf
 ARG1=--public-ip=
 ARG2=--snow-quorum-size=14
 ARG3=--snow-virtuous-commit-threshold=15
 EOF'
 
-sudo USER=$USER bash -c 'cat <<EOF > /etc/systemd/system/avaxnode.service
+sudo USER=$USER bash -c 'cat <<EOF > /etc/systemd/system/avalanche.service
 [Unit]
 Description=Avalanche node service
 After=network.target
@@ -61,7 +51,7 @@ User=$USER
 Group=$USER
 
 WorkingDirectory='$GOPATH'/src/github.com/ava-labs/avalanchego
-EnvironmentFile=/etc/.avaxnodeconf
+EnvironmentFile=/etc/.avalanche.conf
 ExecStart='$GOPATH'/src/github.com/ava-labs/avalanchego/build/avalanchego $ARG2 $ARG3
 
 Restart=always
@@ -75,8 +65,40 @@ StartLimitBurst=5
 WantedBy=multi-user.target
 EOF'
 
+
+echo '### Creating Avalanche auto-update service'
+sudo USER=$USER bash -c 'cat <<EOF > /etc/systemd/system/avaxmonitoring.service
+[Unit]
+Description=Avalanche update monitoring service
+After=network.target
+
+[Service]
+User=$USER
+Group=$USER
+
+WorkingDirectory='$HOME'/bin
+ExecStart='$HOME'/bin/monitoring.sh
+
+Restart=always
+PrivateTmp=true
+TimeoutStopSec=60s
+TimeoutStartSec=10s
+StartLimitInterval=120s
+StartLimitBurst=5
+
+[Install]
+WantedBy=multi-user.target
+EOF'
+
+echo '### Launching Avalanche auto-update service...'
+sudo systemctl enable avaxmonitoring
+sudo systemctl start avaxmonitoring
+
 echo '### Launching Avalanche node...'
-sudo systemctl enable avaxnode
-sudo systemctl start avaxnode
-echo 'Type the following command to monitor the AVAX node service:'
-echo '    sudo systemctl status avaxnode'
+sudo systemctl enable avalanche
+sudo systemctl start avalanche
+
+echo 'Node launched'
+echo 'Type the following command to monitor the Avalanche node service:'
+echo '    sudo systemctl status avalanche'
+echo 'To change the launch arguments, edit the /etc/.avalanche.conf file'
