71da80583c592955f51e4bb38b57cae4c6787e7e
tdeso <48097413+tdeso@users.noreply.github.com>
2020-10-06 18:40:17 +0200

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-journalctl -f -u avaxnode | awk '
-                      /You may want to update your client/ { system ("$HOME/bin/update.sh") }
-                      /<P Chain> snow/engine/snowman/transitive.go#114: bootstrapping finished/ { print "##### P CHAIN SUCCESSFULLY BOOTSTRAPPED" }
-                      /<X Chain> snow/engine/avalanche/transitive.go#98: bootstrapping finished/ { print "##### X CHAIN SUCCESSFULLY BOOTSTRAPPED" }
-                      /<C Chain> snow/engine/snowman/transitive.go#114: bootstrapping finished/ { print "##### C CHAIN SUCCESSFULLY BOOTSTRAPPED" }'
+journalctl -f -u avaxnode -n 0 | awk '
+/You may want to update your client/ { system ("$HOME/bin/update.sh") }
+/<P Chain> snow/engine/snowman/transitive.go#114: bootstrapping finished/ { print "##### P CHAIN SUCCESSFULLY BOOTSTRAPPED" }
+/<X Chain> snow/engine/avalanche/transitive.go#98: bootstrapping finished/ { print "##### X CHAIN SUCCESSFULLY BOOTSTRAPPED" }
+/<C Chain> snow/engine/snowman/transitive.go#114: bootstrapping finished/ { print "##### C CHAIN SUCCESSFULLY BOOTSTRAPPED" }'
