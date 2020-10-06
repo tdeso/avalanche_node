443b27043725a159c0bd7566a89f7377614ab523
tdeso <48097413+tdeso@users.noreply.github.com>
2020-10-06 17:08:55 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+# Bash script to update an Avalanche node that runs as a service named avalanche
+NODE_VERSION1=$(bac -f info.getNodeVersion | grep version | awk 'NR==1 {print $2}' | sed 's/avalanche//' | tr -d '\/"')
 
-################################################
-# Simple Bash script to update an Avalanche Node
-# ##############################################
+echo '      _____               .__                       .__		  '
+echo '     /  _  \___  _______  |  | _____    ____   ____ |  |__   ____   '
+echo '    /  /_\  \  \/ /\__  \ |  | \__  \  /    \_/ ___\|  |  \_/ __ \  '
+echo '   /    |    \   /  / __ \|  |__/ __ \|   |  \  \___|   Y  \  ___/  '
+echo '   \____|__  /\_/  (____  /____(____  /___|  /\___  >___|  /\___  > '
+echo '           \/           \/          \/     \/     \/     \/     \/  '
 
 echo '### Updating packages...'
 sudo apt-get -y update
@@ -13,34 +18,53 @@ git pull
 
 echo '### Updating Avalanche node service...'
 ./scripts/build.sh
-sudo systemctl restart {monitor;avalanche}
 
+if systemctl -a list-units | grep -q 'monitor'; then    
+  sudo systemctl restart monitor    
+fi
+
+sudo systemctl restart avalanche
+
+NODE_VERSION2=$(bac -f info.getNodeVersion | grep version | awk 'NR==1 {print $2}' | sed 's/avalanche//' | tr -d '\/"')
 NODE_STATUS=$(sudo systemctl status avalanche | grep Active | awk 'NR==1 {print $2}' | tr -d \")
-if [[ "$NODE_STATUS" == "active" ]]
-    then
-    echo ''
-    echo "${bold}##### AVALANCHE NODE SUCCESSFULLY UPDATED #####${normal}"
-    echo ''
-    echo 'To monitor the Avalanche node service, type the following commands:'
-    echo '    sudo systemctl status avalanche'
-    echo '    journalctl -u avalanche'
-    echo 'To change the node launch arguments, edit the following file:'
-    echo '    /etc/.avalanche.conf'
-    echo 'To monitor the node monitoring service, type the following commands:'
-    echo '    sudo systemctl status monitor'
-    echo '    journalctl -u monitor'
-    echo ''
-elif [[ "$NODE_STATUS" = "failed" ]]
-    then
-    echo '${bold}##### AVALANCHE NODE UPDATE FAILED #####{normal}'
-    echo ''
-    echo 'To monitor the Avalanche node service, type the following commands:'
-    echo '    sudo systemctl status avalanche'
-    echo '    journalctl -u avalanche'
-    echo 'To change the node launch arguments, edit the following file:'
-    echo '    /etc/.avalanche.conf'
-    echo 'To monitor the node monitoring service, type the following commands:'
-    echo '    sudo systemctl status monitor'
-    echo '    journalctl -u monitor'
-    echo ''
+
+if [[ "$NODE_STATUS" == "active" ]] && [[ "$NODE_VERSION1" != "$NODE_VERSION2" ]]; then
+  echo ''
+  echo "${bold}##### AVALANCHE NODE SUCCESSFULLY UPDATED TO $NODE_VERSION #####${normal}"
+  echo ''
+  echo 'To monitor the Avalanche node service, type the following commands:'
+  echo '    sudo systemctl status avalanche'
+  echo '    journalctl -u avalanche'
+  echo 'To change the node launch arguments, edit the following file:'
+  echo '    /etc/.avalanche.conf'
+  echo 'To monitor the node monitoring service, type the following commands:'
+  echo '    sudo systemctl status monitor'
+  echo '    journalctl -u monitor'
+  echo ''
+elif [[ "$NODE_STATUS" == "active" ]] && [[ "$NODE_VERSION1" != "$NODE_VERSION2" ]]; then
+  echo ''
+  echo "${bold}##### AVALANCHE NODE UPDATE FAILED #####{normal}"
+  echo ''
+  echo 'To monitor the Avalanche node service, type the following commands:'
+  echo '    sudo systemctl status avalanche'
+  echo '    journalctl -u avalanche'
+  echo 'To change the node launch arguments, edit the following file:'
+  echo '    /etc/.avalanche.conf'
+  echo 'To monitor the node monitoring service, type the following commands:'
+  echo '    sudo systemctl status monitor'
+  echo '    journalctl -u monitor'
+  echo ''
+elif [[ "$NODE_STATUS" == "failed" ]]; then
+  echo ''
+  echo "${bold}##### AVALANCHE NODE FAILED TO LAUNCH #####{normal}"
+  echo ''
+  echo 'To monitor the Avalanche node service, type the following commands:'
+  echo '    sudo systemctl status avalanche'
+  echo '    journalctl -u avalanche'
+  echo 'To change the node launch arguments, edit the following file:'
+  echo '    /etc/.avalanche.conf'
+  echo 'To monitor the node monitoring service, type the following commands:'
+  echo '    sudo systemctl status monitor'
+  echo '    journalctl -u monitor'
+  echo ''
 fi
